244fde5c73ee5a2dfc1b7ba7a9c7f7e9864e85b5
Mendelsohn Chan <mendelsohn.chan@torontomu.ca>
2026-06-06 14:56:39 +0000

    dd

--- models/t2-silver/t2_silver_table.sql
+++ models/t2-silver/t2_silver_table.sql
@@ -2,7 +2,7 @@
 -- Use the `ref` function to select from other models
 
 select  
-    order_date AS order__date,
+    order_date AS ordered_date,
     sales AS sales_revenue
 from {{ ref('t1_bronze_table') }}
 where order_date is not null 
--- models/t3-gold/t3_gold_table.sql
+++ models/t3-gold/t3_gold_table.sql
@@ -2,8 +2,8 @@
 -- Use the `ref` function to select from other models
 
 select  
-    order__date,
+    ordered_date,
     sum(sales_revenue) as total_revenue
 from {{ ref('t2_silver_table') }}
+where order_date is not null
 group by 1
-where order_date  is not null 
