82854e0b2bd7d8ce3031fada0bbd1903726a40ff
Mendelsohn Chan <mendelsohn.chan@torontomu.ca>
2026-06-06 14:57:28 +0000

    d

--- models/t3-gold/t3_gold_table.sql
+++ models/t3-gold/t3_gold_table.sql
@@ -1,9 +1,6 @@
-
--- Use the `ref` function to select from other models
-
 select  
     ordered_date,
     sum(sales_revenue) as total_revenue
 from {{ ref('t2_silver_table') }}
-where order_date is not null
+where ordered_date is not null
 group by 1
